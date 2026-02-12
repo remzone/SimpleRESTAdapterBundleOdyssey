d03c4dba37fb1a8de512aafd6509b764fc98b553
remzone <155640872+remzone@users.noreply.github.com>
2026-02-12 14:02:04 +0300

    Update startup.js

--- src/SimpleRESTAdapterBundle/Resources/public/pimcore/js/startup.js
+++ src/SimpleRESTAdapterBundle/Resources/public/pimcore/js/startup.js
@@ -1,25 +1,63 @@
 /**
- * Simple REST Adapter.
- *
- * LICENSE
- *
- * This source file is subject to the GNU General Public License version 3 (GPLv3)
- * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
- * files that are distributed with this source code.
- *
- * @copyright  Copyright (c) 2021 CI HUB GmbH (https://ci-hub.com)
- * @license    https://github.com/ci-hub-gmbh/SimpleRESTAdapterBundle/blob/master/gpl-3.0.txt GNU General Public License version 3 (GPLv3)
+ * Simple REST Adapter (Pimcore 11 compatibility bootstrap).
  */
 
-pimcore.registerNS('pimcore.plugin.simpleRestAdapterBundle');
-pimcore.plugin.simpleRestAdapterBundle = Class.create(pimcore.plugin.admin, {
-    getClassName: function () {
-        return 'pimcore.plugin.simpleRestAdapterBundle';
-    },
+pimcore.registerNS("pimcore.plugin.simpleRestAdapterBundle");
 
-    initialize: function () {
-        pimcore.plugin.broker.registerPlugin(this);
-    },
+pimcore.plugin.simpleRestAdapterBundle = Class.create(pimcore.plugin.admin, {
+  getClassName: function () {
+    return "pimcore.plugin.simpleRestAdapterBundle";
+  },
+
+  initialize: function () {
+    pimcore.plugin.broker.registerPlugin(this);
+
+    // Ensure our DataHub adapter JS is loaded in admin
+    this.loadDependencies();
+  },
+
+  loadDependencies: function () {
+    const base = "/bundles/simplerestadapter/pimcore/js/";
+
+    const files = [
+      "config-item.js",
+      "grid-config-dialog.js",
+      "adapter.js",
+    ];
+
+    // Load sequentially to keep Class.create dependencies safe
+    const loadNext = (idx) => {
+      if (idx >= files.length) {
+        return;
+      }
+
+      const url = base + files[idx];
+
+      // already loaded?
+      if (document.querySelector(`script[data-sra="${files[idx]}"]`)) {
+        loadNext(idx + 1);
+        return;
+      }
+
+      const s = document.createElement("script");
+      s.type = "text/javascript";
+      s.async = false;
+      s.src = url;
+      s.setAttribute("data-sra", files[idx]);
+
+      s.onload = () => loadNext(idx + 1);
+      s.onerror = () => {
+        // eslint-disable-next-line no-console
+        console.error("[SimpleRESTAdapterBundle] Failed to load", url);
+        loadNext(idx + 1);
+      };
+
+      document.head.appendChild(s);
+    };
+
+    loadNext(0);
+  },
 });
 
+new pimcore.plugin.simpleRestAdapterBundle();
 const simpleRestAdapterBundle = new pimcore.plugin.simpleRestAdapterBundle();
